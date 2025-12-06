0188201dfa4152b54fa93bf28364ec3871b8182b
CHETAN <127501995+KASHYAPCHETAN438@users.noreply.github.com>
2025-12-06 17:37:56 +0530

    Frontend

--- Frontend/pages/Signup.tsx
+++ Frontend/pages/Signup.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import '../styles/auth.css';
+// import '../styles/auth.css';
 
 const Signup: React.FC = () => {
   const [name, setName] = useState('');
